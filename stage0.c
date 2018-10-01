31ce36805b0cf3a1578e1e73b8e96f6c406a747a
Yohann Leon <yohann@leon.re>
2018-10-01 19:57:48 +0200

    int15: ax=0xe820 correctly initialized

--- stage0.c
+++ stage0.c
@@ -12,17 +12,33 @@ void prints(char *chr) {
         printc(*chr++);
 }
 
+// return true if init is successful
 uint8_t get_a20() {
     uint8_t a = 0;
     __asm__ volatile ("int $0x15":"=r"(a):"a"(0x2402));
     return a;
 }
 
+// return true if a20 is initialized
 void set_a20(uint8_t status) {
     if (status)
         __asm__ volatile ("int $0x15"::"a"(0x2401));
     else
         __asm__ volatile ("int $0x15"::"a"(0x2400));
+}
+
+// return true if init is successful
+uint8_t find_ram() {
+    uint8_t flags = 0;
+    __asm__ volatile ("int $0x15":"=r"(flags):
+            "a"(0xe820),
+            "b"(0x0),
+            "c"(0x20),
+            "d"(0x534d4150));
+
+    // get cf flag
+    // cf clear if successful and set on error
+    return !(flags & 0x1);
 }
 
 uint8_t read_disk(uint8_t nb_sector) {
@@ -36,7 +52,7 @@ uint8_t read_disk(uint8_t nb_sector) {
     //
     // al // number of sector read
     uint8_t read_sector = 0;
-    __asm__ volatile ("int $0x13":"=a"(read_sector):
+    __asm__ volatile ("int $0x13":"=r"(read_sector):
             "a"(0x0200 | nb_sector),
             "b"(&begin_addr),
             "c"(0x2),
@@ -51,16 +67,22 @@ void stage0() {
 
     set_a20(1);
 
-    if (get_a20() == 1)
+    if (get_a20())
     {
         char a20[] = "/a20";
         prints(a20);
     }
 
+    if (find_ram())
+    {
+        char ram[] = "/ram";
+        prints(ram);
+    }
+    
     char read[] = "/read";
     prints(read);
 
-    uint8_t read_nb = read_disk(4);
+    uint8_t read_nb = read_disk(5);
     printc(read_nb + 48);
 
     stage1();
