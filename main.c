7531b3f1ad850b572671dc3b75961d92ad1ab241
Yohann Leon <yohann@leon.re>
2018-10-01 16:02:21 +0200

    int13: wip on sector reading

--- main.c
+++ main.c
@@ -18,6 +18,21 @@ void set_a20(uint8_t status) {
         __asm__ volatile ("int $0x15"::"a"(0x2400));
 }
 
+uint8_t secte() {
+
+    // ah = 0x02 // bios read sector function
+    // al = 0x04 // number of sector to read
+    // ch = 0x00 // select cylinder 0
+    // cl = 0x02 // read second sector
+    // dh = 0x00 // select head 0
+    // dl = 0x00 // drive number
+    //
+    // al // number of sector read
+    uint8_t read_nb = 0;
+    __asm__ volatile ("int $0x13":"=a"(read_nb):"a"(0x2 << 8 & 0x4),"c"(0x2),"d"(0x0));
+    return read_nb;
+}
+
 void start() {
     printc('b'); 
 
@@ -27,6 +42,9 @@ void start() {
         printc('y'); 
     else
         printc('n'); 
+
+    uint8_t read_nb = secte();
+    printc(read_nb + 48);
 
     return;
 }
