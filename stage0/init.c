9ebb2268aca97c7e78080475b29aadd5932eed85
Corentin Giroud-Argoud <giroud_c@epita.fr>
2018-10-08 21:21:20 +0200

    test loading kernel at 0x100000

--- stage0/init.c
+++ stage0/init.c
@@ -23,12 +23,15 @@ static void set_a20(uint8_t status) {
         __asm__ volatile ("int $0x15"::"a"(0x2400));
 }
 
-static uint8_t read_disk(uint16_t begin, uint8_t nb_sector, uint8_t sector) {
+static uint8_t read_disk(uint8_t nb_sector, uint8_t sector) {
     uint8_t read_sector = 0;
+
+    __asm__ volatile ("mov $0xffff, \%ax");
+    __asm__ volatile ("mov \%ax, \%es");
 
     __asm__ volatile ("int $0x13":"=r"(read_sector):
             "a"(0x0200 | nb_sector),
-            "b"(begin),
+            "b"(0x1000),
             "c"(0x0000 | sector),
             "d"(0x0));
 
@@ -39,8 +42,13 @@ void stage0() {
     set_a20(1);
 
     uint8_t read_nb = 2;
-    read_nb += read_disk(&_stage1_begin,
+    read_nb += read_disk(
             (STAGE1_SIZE + STAGE2_SIZE + STAGE3_SIZE) / 512, read_nb);
 
-    gdt();
+    __asm__ volatile("mov $0x1000, \%ax");
+    __asm__ volatile("mov \%ax, \%es");
+    __asm__ volatile("mov $0x0, \%bx");
+    __asm__ volatile("push \%es");
+    __asm__ volatile("push \%bx");
+    __asm__ volatile("retf");
 }
